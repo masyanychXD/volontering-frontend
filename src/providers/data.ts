676ff8e7f9a110b04ea4cf9c35c5f6f2f3578307
masyanychXD <144156122+masyanychXD@users.noreply.github.com>
2026-06-07 21:36:24 +0300

    add toast for rate limiting

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,7 +1,24 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import {BACKEND_BASE_URL} from "@/constants";
 import {ListResponse} from "@/types";
+import {HttpError} from "@refinedev/core";
 
+const buildHttpError = async (response: Response): Promise<HttpError> => {
+  let message: string = 'Request failed';
+
+  try {
+    const payload = (await response.json()) as { message?: string };
+
+    if (payload?.message) message = payload.message;
+  } catch (e) {
+    // Ignore errors
+  }
+
+  return {
+    message,
+    statusCode: response.status,
+  }
+}
 
 const options: CreateDataProviderOptions = {
   getList: {
@@ -28,12 +45,16 @@ const options: CreateDataProviderOptions = {
     },
 
     mapResponse: async (response) => {
+      if(!response.ok) throw await buildHttpError(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.data ?? [];
     },
 
     getTotalCount: async (response) => {
+      if(!response.ok) throw await buildHttpError(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
